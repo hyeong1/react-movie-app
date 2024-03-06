24b6c0b6b8678faaebd34fb212e401b8d2deb31f
hyeong1 <mi75625@gmail.com>
2024-03-06 16:08:49 +0900

    chore: url 경로 수정

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/react-movie-app" element={<Home />} />
-        <Route path="/react-movie-app/movie/:id" element={<Detail />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/movie/:id" element={<Detail />} />
       </Routes>
     </Router>
   );
